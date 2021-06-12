a101acd1eca16a4830f8df39e66dd1be25565998
Cameron Chandler <webstormcam@gmail.com>
2021-06-12 06:35:34 -0400

    Made so the content can be opened and closed

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const opener=document.getElementById('open-modal')
+const closer=document.getElementById('close-modal')
 const overlayy=document.getElementById('overlay')
+
 
 opener.addEventListener('click',function(){
 overlayy.style.display='block'
+})
+
+closer.addEventListener('click',function(){
+    overlayy.style.display='none'
 })
