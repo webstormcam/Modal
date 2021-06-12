0fc5dfe188b8de4a2e733e2c2e9456714c902401
Cameron Chandler <webstormcam@gmail.com>
2021-06-12 06:28:07 -0400

    Made the modal come out of hiding

---
+++ index.js
@@ -0,0 +1,6 @@
+const opener=document.getElementById('open-modal')
+const overlayy=document.getElementById('overlay')
+
+opener.addEventListener('click',function(){
+overlayy.style.display='block'
+})
